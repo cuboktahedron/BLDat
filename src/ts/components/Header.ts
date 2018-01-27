e3152d7cb3c9469f875488008fa2877fcb453bd7
cuboktahedron <cuboktahedron@gmail.com>
2018-01-27 14:42:20 +0900

    add external links on header

--- src/ts/components/Header.ts
+++ src/ts/components/Header.ts
@@ -1,5 +1,7 @@
 import Vue, { ComponentOptions } from 'vue'
 
+import * as octicons from 'octicons'
+
 interface Header extends Vue {
 }
 
@@ -10,9 +12,18 @@ export default {
 
   template: `
     <div id="header">
-      <a href="https://twitter.com/cubokta" target="_blank">
-        <img class="twitter" src="http://localhost:1313/img/twitter-icon.png">
-      </a>
+      <a class="icon" href="https://twitter.com/cubokta" target="_blank" title="twitter">
+        <img class="twitter" src="https://raw.githubusercontent.com/cuboktahedron/resources/master/imgs/twitter-icon.png">
+      </a><a class="icon" href="https://github.com/cuboktahedron/BLDat" target="_blank"
+        title="github" v-html="markGithub" />
     </div>`,
 
+    computed: {
+      markGithub() {
+        const icon = octicons['mark-github'];
+        return icon.toSVG({
+          'width': 32,
+        });
+      }
+    }
 } as ComponentOptions<Header>
